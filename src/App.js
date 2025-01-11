3e7087c1c720eb6813745cc3366df192ecfe6b2d
Kirill Pavlov <Stricker1990@gmail.com>
2025-01-11 20:58:09 +0400

    grid template

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.scss';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <header>
+        header
       </header>
       <main>
         <test>Test</test>
       </main>
+      <footer>
+        footer
+      </footer>
     </div>
   );
 }
