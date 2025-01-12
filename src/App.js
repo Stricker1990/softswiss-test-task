74845dea3bbd98cb7388617ddaa688af2cf20b94
Kirill Pavlov <Stricker1990@gmail.com>
2025-01-12 15:13:08 +0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function App() {
   return (
     <div className="container">
       <header>
-        header
+      <nav class="navbar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">
+            <svg height="20" width="30" xmlns="http://www.w3.org/2000/svg">
+              <image height="20" width="30" href="cart.svg" alt="Cart"/>
+            </svg>
+          </a></li>
+        </ul>
+      </nav>
       </header>
       <main>
         <test>Test</test>
