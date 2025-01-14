d9ffdb4d6347a20bc79ec9f6d13eca9d28053d41
Kirill Pavlov <Stricker1990@gmail.com>
2025-01-15 01:23:12 +0400

    offers

--- src/App.js
+++ src/App.js
@@ -70,9 +70,9 @@ function MainBanner() {
 
 function Offers() {
   return (
-    <div>
-      <h1 className="white padding-lr-2">Offers</h1>
-      <div className="offers-container padding-lr-2">
+    <div className="white padding-2">
+      <h1>Offers</h1>
+      <div className="offers-container">
         <div className="offers-container__item_large">
           <BannerOffer1 />
         </div>
@@ -86,6 +86,11 @@ function Offers() {
           <BannerOffer4 />
         </div>
       </div>
+      <h3>Embark on a space journey</h3>
+      <p className="opacity-60">
+        Travelling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible impressions, excitement and new discoveries. Our team of professionals takes care of your safety and comfort so that you can fully enjoy your adventure in space. We offer various options for space excursions.
+      </p>
+      <button className="button button_link">Read more</button>
     </div>
   );
 }
